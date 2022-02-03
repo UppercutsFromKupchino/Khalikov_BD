27fe589d804a3d0360c4a5edda6fb0d6ff630342
Jerrehy <sacha15012000@gmail.com>
2022-02-03 21:33:55 +0300

    Test commit

--- app/__init__.py
+++ app/__init__.py
@@ -7,3 +7,12 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 db = SQLAlchemy(app)
+db.Model.metadata.reflect(db.engine)
+
+# login_manager = LoginManager(app)
+# login_manager.login_view = 'login_page'
+# login_manager.login_message_category = 'info'
+# login_manager.login_message = 'Пожалуйста, выполните вход для дальнейших действий'
+
+# Место для подключения узлов
+from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,18 @@
 from app import app
 from flask import render_template, flash, url_for, session
-from flask_login import LoginManager
+# from flask_login import login_user, logout_user
 
-
-login_manager = LoginManager(app)
-
-@login_manager.init_app(app)
-def load_user():
-    return
 
 @app.route('/')
 def index():
+    return render_template("index.html")
+
+
+@app.route('/login')
+def login():
+    return render_template("index.html")
+
+
+@app.route('/register')
+def register():
     return render_template("index.html")
