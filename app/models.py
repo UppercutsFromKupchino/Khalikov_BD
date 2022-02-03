e5659835ba6e83031573ccd2663bcff4eb4261f6
Jerrehy <sacha15012000@gmail.com>
2022-02-03 22:12:14 +0300

    Base models ver1

--- app/models.py
+++ app/models.py
@@ -14,7 +14,49 @@ class User(db.Model, UserMixin):
     __table_args__ = {'extend_existing': True}
 
     id_of_user = db.Column(db.Integer(), primary_key=True)
+    role_of_user = db.relationship('RoleOfUser', backref='role_of_user', uselist=False)
 
     # Метод получения ID пользователя из таблицы
     def get_id(self):
         return self.id_of_user
+
+
+class Order(db.Model):
+    __tablename__ = '_order'
+    __table_args__ = {'extend_existing': True}
+
+    id_of_consumer = db.relationship('User', backref='_user', uselist=False)
+
+
+class Ad(db.Model):
+    __tablename__ = 'adr'
+    __table_args__ = {'extend_existing': True}
+
+    id_of_executor = db.relationship('User', backref='_user', uselist=False)
+
+
+class ConsumerOfAd(db.Model):
+    __tablename__ = 'consumer_of_ad'
+    __table_args__ = {'extend_existing': True}
+
+    id_of_consumer = db.relationship('User', backref='_user', uselist=False)
+
+
+class ExecutorOfAd(db.Model):
+    __tablename__ = 'executor_of_ad'
+    __table_args__ = {'extend_existing': True}
+
+    id_of_executor = db.relationship('User', backref='_user', uselist=False)
+
+
+class Feedback(db.Model):
+    __tablename__ = 'feedback'
+    __table_args__ = {'extend_existing': True}
+
+    id_of_user = db.relationship('User', backref='_user', uselist=False)
+
+
+class RoleOfUser(db.Model):
+    __tablename__ = 'role_of_user'
+    __table_args__ = {'extend_existing': True}
+
